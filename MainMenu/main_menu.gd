91e90a93ded5a2a10813c10a668974e8ef8b7be5
will <will.l.handel@gmail.com>
2026-02-21 18:08:17 -0500

    Connected UIs in the main menu to buttons to display and back out of menus. Allowed for the quitting and starting of a new game.

---
+++ MainMenu/main_menu.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+var currentMenu: Node
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/mainLevel.tscn")
+	pass
+
+
+func _on_load_data_button_pressed() -> void:
+	$UI/Back.visible = true
+	currentMenu = $UI/Load
+	$UI/MainButtons.visible = false
+	$UI/Load.visible = true
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	$UI/Back.visible = true
+	currentMenu = $UI/Settings
+	$UI/MainButtons.visible = false
+	$UI/Settings.visible = true
+	pass
+
+
+func _on_learning_center_button_pressed() -> void:
+	$UI/Back.visible = true
+	currentMenu = $UI/LearningCenter
+	$UI/MainButtons.visible = false
+	$UI/LearningCenter.visible = true
+	pass
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	pass
+
+
+func _on_back_pressed() -> void:
+	currentMenu.visible = false
+	$UI/Back.visible = false
+	$UI/MainButtons.visible = true
+	pass
