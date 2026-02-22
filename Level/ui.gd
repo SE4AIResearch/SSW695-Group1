ba4dfde4ec2a45b950a64e6b8dadcd53cb2e886f
will <will.l.handel@gmail.com>
2026-02-21 19:17:14 -0500

    Added button functionality to main game buttons and pause buttons.

---
+++ Level/ui.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+var currentMenu: Node
+
+var UpgradesMenu = load("res://UI/InGame/Upgrades/Upgrades.tscn")
+var ProjectMetricsMenu = load("res://UI/InGame/ProjectMetrics/ProjectMetrics.tscn")
+var HiringMenu = load("res://UI/InGame/Hiring/Hiring.tscn")
+var BacklogMenu = load("res://UI/InGame/Backlog/Backlog.tscn")
+
+
+func _ready() -> void:
+	pass
+
+
+
+
+func _on_pause_button_pressed() -> void:
+	match get_tree().paused:
+		true: $Pause.resumePaused = true
+		false: $Pause.resumePaused = false
+	get_tree().paused = true
+	$Pause.visible = true
+	pass
+
+
+func _on_back_button_pressed() -> void:
+	currentMenu.queue_free()
+	$BackButton.visible = false
+	get_tree().paused = false
+	pass
+
+func _on_upgrades_button_pressed() -> void: createMenu(UpgradesMenu)
+
+func _on_project_metrics_button_pressed() -> void: createMenu(ProjectMetricsMenu)
+
+func _on_hiring_button_pressed() -> void: createMenu(HiringMenu)
+
+func _on_backlog_button_pressed() -> void: createMenu(BacklogMenu)
+
+func createMenu(NewMenu):
+	var menu = NewMenu.instantiate()
+	$NewMenu.add_child(menu)
+	currentMenu = menu
+	get_tree().paused = true
+	currentMenu.visible = true
+	$BackButton.visible = true
---
+++ UI/InGame/Pause/pause.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var resumePaused: bool
+var currentMenu: Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	self.visible = false
+	match resumePaused:
+		true: get_tree().paused = true
+		false: get_tree().paused = false
+	pass
+
+
+func _on_quit_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
+	pass
+
+
+func _on_learning_centerbutton_pressed() -> void:
+	currentMenu = $LearningCenter
+	currentMenu.visible = true
+	$Menu.visible = false
+	$Back.visible = true
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	currentMenu = $Settings
+	currentMenu.visible = true
+	$Menu.visible = false
+	$Back.visible = true
+	pass
+
+
+func _on_back_pressed() -> void:
+	currentMenu.visible = false
+	$Menu.visible = true
+	pass
