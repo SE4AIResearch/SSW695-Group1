dc6c6245537c7f306bd3a8f11ae706a7f114d9c4
will <will.l.handel@gmail.com>
2026-02-24 16:06:07 -0500

    Person and worker visual and name setup.

---
+++ Person/Worker/worker.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var personName: String
+
+var headSpritePath: String
+var hairSpritePath: String
+var facialHairSpritePath: String
+var mouthSpritePath: String
+var noseSpritePath: String
+
+var frontEndWorkerStat: int
+var backEndWorkerStat: int
+var documentingWorkerStat: int
+var speedWorkerStat: int
+var staminaWorkerStat: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Person/person.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var personName: String
+
+var headSpritePath: String
+var hairSpritePath: String
+var facialHairSpritePath: String
+var mouthSpritePath: String
+var noseSpritePath: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
