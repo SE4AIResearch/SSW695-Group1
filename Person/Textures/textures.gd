e12c052a9d663caf20e482e718c9f875b591cf45
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-02-26 17:37:21 -0500

    Textures

---
+++ Person/Textures/textures.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var eyes = [
+	"res://Person/Textures/Eyes/01.png",
+	"res://Person/Textures/Eyes/02.png",
+	"res://Person/Textures/Eyes/03.png",
+]
+
+var hair = [
+	"res://Person/Textures/Hair/01.png",
+	"res://Person/Textures/Hair/02.png",
+	"res://Person/Textures/Hair/03.png",
+	"res://Person/Textures/Hair/04.png",
+]
+
+var head = [
+	"res://Person/Textures/Head/01.png",
+]
+
+var mouth = [
+	"res://Person/Textures/Mouth/01.png",
+	"res://Person/Textures/Mouth/02.png",
+	"res://Person/Textures/Mouth/03.png",
+	"res://Person/Textures/Mouth/04.png",
+	"res://Person/Textures/Mouth/05.png",
+]
+
+var nose = [
+	"res://Person/Textures/Nose/01.png",
+	"res://Person/Textures/Nose/02.png",
+	"res://Person/Textures/Nose/03.png",
+	"res://Person/Textures/Nose/04.png",
+]
