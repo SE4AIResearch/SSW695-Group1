dd0092156dddf7b9ecb1c764386bc14c1c5bc67a
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-03-19 18:43:01 -0400

    Minor Adjustments

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -73,5 +73,5 @@ func setButtonVisual(menuButton: Button):
 	menuButton.add_theme_stylebox_override("normal",normalStylebox)
 	menuButton.add_theme_stylebox_override("hover",hoverStylebox)
 	menuButton.add_theme_stylebox_override("pressed",pressedStylebox)
-	menuButton.modulate = Color(randf_range(.6,1),randf_range(.6,1),randf_range(.6,1))
+	menuButton.modulate = Color(randf_range(.5,1),randf_range(.5,1),randf_range(.5,1))
 	pass
