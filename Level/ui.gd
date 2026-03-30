a13908a2f75c355078683b8df2be038186e2fba5
will <will.l.handel@gmail.com>
2026-03-30 19:47:19 -0400

    Quitting to menu resets player data.

--- Level/ui.gd
+++ Level/ui.gd
@@ -7,19 +7,14 @@ var HiringMenu = load("res://UI/InGame/Hiring/Hiring.tscn")
 var BacklogMenu = load("res://UI/InGame/Backlog/Backlog.tscn")
 var ProjectSetupMenu = load("res://UI/InGame/ProjectSetup/ProjectSetup.tscn")
 var randomEventMenu = load("res://UI/InGame/RandomEvent/RandomEvent.tscn")
-var workerScene = preload("res://Person/Worker/Worker.tscn")
 
 var pcMode = false
-var deskWorker: Node2D
 
-const DESK_WORKER_POSITION := Vector2(225, 375)
-const DESK_WORKER_SCALE := Vector2(4.5, 4.5)
-const DESK_WORKER_Z_INDEX := 0
+
 
 func _ready() -> void:
 	PlayerTool.connect("projectSelected",toggleProjectButtons)
 	PlayerTool.connect("projectFinished",toggleProjectButtons)
-	createDeskWorker()
 	toggleProjectButtons()
 
 func _physics_process(delta: float) -> void:
@@ -107,21 +102,6 @@ func _on_pc_power_pressed() -> void:
 	if $NewMenu.get_children().size() > 0: $NewMenu.get_child(0).queue_free()
 	pass
 
-func createDeskWorker() -> void:
-	deskWorker = workerScene.instantiate()
-	deskWorker.personName = PersonConstructor.generateName()
-	PersonConstructor.generateVisuals(deskWorker)
-	PersonConstructor.generateWorkerStats(deskWorker)
-	deskWorker.position = DESK_WORKER_POSITION
-	deskWorker.scale = DESK_WORKER_SCALE
-	deskWorker.z_index = DESK_WORKER_Z_INDEX
-	deskWorker.visible = false
-	add_child(deskWorker)
-	move_child(deskWorker, $DeskLaptopOpen.get_index())
-
 func toggleProjectButtons():
 	var hasProject = PlayerTool.currentProject != null
 	$BacklogButton.disabled = !hasProject
-	$DeskLaptopOpen.visible = hasProject
-	if deskWorker != null:
-		deskWorker.visible = hasProject
--- UI/InGame/Pause/pause.gd
+++ UI/InGame/Pause/pause.gd
@@ -25,6 +25,7 @@ func _on_resume_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
+	PlayerTool.resetData()
 	pass
 
 
