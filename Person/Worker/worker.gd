2301d5198a30f209e10f5c13bbcf34a2d31917c0
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-03-31 19:43:59 -0400

    worker metric tracking setup.

--- Person/Worker/worker.gd
+++ Person/Worker/worker.gd
@@ -16,11 +16,7 @@ var staminaStat: int
 
 var firable: bool = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var currentMetric={
+"metricType":null,
+"metricName":null
+}
