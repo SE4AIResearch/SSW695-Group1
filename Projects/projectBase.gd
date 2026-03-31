f3df19cdcb12c392d9deb203fdef1a68f4c89927
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-03-31 17:44:20 -0400

    Rid of stats that didnt matter, added stats that do.

--- Projects/projectBase.gd
+++ Projects/projectBase.gd
@@ -5,21 +5,21 @@ var projectDescription: String
 
 var clientName: String
 
-var frontEndScalar: float
-var backEndScalar: float
-var documentingScalar: float
-
 var frontEndProjectMin: int
 var backEndProjectMin: int
 var documentingProjectMin: int
 
 var baseSprintAmount: int
-var sprintAmount: int
-
 var baseSprintLength: int
+var baseSprintMetricAmount: int
+
+var sprintAmount: int
 var sprintLength: int
+var sprintMetricAmount: int
 
 var methodology: Dictionary
 var constraints: Dictionary
 
-var metric: Dictionary
+var frontEndMetrics: Dictionary
+var backEndMetrics: Dictionary
+var documentingMetrics: Dictionary
