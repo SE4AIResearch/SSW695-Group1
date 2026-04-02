5e6abf5b8683cfeeac4c1ec4c09cd8e95c6f5467
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-04-02 18:40:03 -0400

    removed print line

--- Tools/numberVisualizerTool/number.gd
+++ Tools/numberVisualizerTool/number.gd
@@ -8,7 +8,6 @@ func setupVisual(amount,color):
 	$numberLabel.text = "[color="+color+"]"+"+"+str(amount)
 
 func moveNumb():
-	print("Moving")
 	position.y -= .4
 	modulate -= Color(0,0,0,.02)
 	if modulate.a <= 0:
