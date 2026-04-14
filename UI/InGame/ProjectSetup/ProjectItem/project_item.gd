768b30e1150232859cce9d48e7496c5fcfd4fdc0
will <will.l.handel@gmail.com>
2026-04-13 21:58:33 -0400

    Made text bold for formatting.

--- UI/InGame/ProjectSetup/ProjectItem/project_item.gd
+++ UI/InGame/ProjectSetup/ProjectItem/project_item.gd
@@ -5,7 +5,7 @@ var heldProject: Node
 
 func prepProject(project):
 	heldProject = project
-	$ProjectDetails.text = "[b]Project:[/b] " +project.projectName + "\n" + "Description: " + project.projectDescription + "\n" + "Client: " + project.clientName + "\n" + "Sprint Amount: " + str(project.sprintAmount) + "\n"+"Weeks per Sprint: " + str(project.sprintLength)
+	$ProjectDetails.text = "[b]Project:[/b] " +project.projectName + "\n" + "[b]Description:[/b] " + project.projectDescription + "\n" + "[b]Client:[/b] " + project.clientName + "\n" + "[b]Sprint Amount:[/b] " + str(project.sprintAmount) + "\n"+"[b]Weeks per Sprint:[/b] " + str(project.sprintLength)
 	$ProjectDetails.text += "\nProject Constraints:"
 	for constraint in project.constraints:
 		$ProjectDetails.text +="\n	-" + constraint.get("name")
