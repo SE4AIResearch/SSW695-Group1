8e8b0923257e10a0284d547265a03816899dd053
will <will.l.handel@gmail.com>
2026-04-13 21:21:07 -0400

    Project choice clarity.

--- UI/InGame/ProjectSetup/ProjectItem/project_item.gd
+++ UI/InGame/ProjectSetup/ProjectItem/project_item.gd
@@ -5,7 +5,7 @@ var heldProject: Node
 
 func prepProject(project):
 	heldProject = project
-	$ProjectDetails.text = "Project:" +project.projectName + "\n" + "Description: " + project.projectDescription + "\n" + "Client: " + project.clientName + "\n" + "Sprint Amount: " + str(project.sprintAmount) + "\n"+"Weeks per Sprint: " + str(project.sprintLength) +"\n"+"Metrics Per Sprint: "+str(project.sprintMetricAmount)+ "\n"+"Front End: " + str(project.frontEndProjectMin) + "\n"+"Back End: " + str(project.backEndProjectMin) + "\n"+"Documenting: "+str(project.documentingProjectMin)
+	$ProjectDetails.text = "[b]Project:[/b] " +project.projectName + "\n" + "Description: " + project.projectDescription + "\n" + "Client: " + project.clientName + "\n" + "Sprint Amount: " + str(project.sprintAmount) + "\n"+"Weeks per Sprint: " + str(project.sprintLength)
 	$ProjectDetails.text += "\nProject Constraints:"
 	for constraint in project.constraints:
 		$ProjectDetails.text +="\n	-" + constraint.get("name")
