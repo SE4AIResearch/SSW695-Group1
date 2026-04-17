4ec45ac84bce5dc6b64de1e3c5d538cb59e9065e
will <will.l.handel@gmail.com>
2026-04-17 16:25:43 -0400

    Project Metrics fully influenced by backlog progress. Workers no longer improve metrics over time.

--- Person/Worker/worker.gd
+++ Person/Worker/worker.gd
@@ -35,19 +35,20 @@ func _on_hover_area_mouse_exited() -> void:
 	hover_ended.emit(self)
 
 func work():
-	if PlayerTool.project != null:
-		var type: int = randi_range(0,2)
-		var amount: int
-		match type:
-			0: 
-				amount = 1#frontEndStat
-				PlayerTool.changeProjectStats(type,amount)
-			1: 
-				amount = 1#backEndStat
-				PlayerTool.changeProjectStats(type,amount)
-			2: 
-				amount = 1#documentingStat
-				PlayerTool.changeProjectStats(type,amount)
-
-		NumberVisualizer.createNumber(amount,type,self.global_position+Vector2(randf_range(-30,30),randf_range(-20,-40)))
-		
+	pass
+#	if PlayerTool.project != null:
+#		var type: int = randi_range(0,2)
+#		var amount: int
+#		match type:
+#			0: 
+#				amount = 1#frontEndStat
+#				PlayerTool.changeProjectStats(type,amount)
+#			1: 
+#				amount = 1#backEndStat
+#				PlayerTool.changeProjectStats(type,amount)
+#			2: 
+#				amount = 1#documentingStat
+#				PlayerTool.changeProjectStats(type,amount)
+#
+#		NumberVisualizer.createNumber(amount,type,self.global_position+Vector2(randf_range(-30,30),randf_range(-20,-40)))
+#		
