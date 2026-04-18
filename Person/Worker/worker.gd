079e297b9175205816f0f4388acfa6ca842222c8
will <will.l.handel@gmail.com>
2026-04-18 15:27:33 -0400

    Worker Stamina Implementation

--- Person/Worker/worker.gd
+++ Person/Worker/worker.gd
@@ -17,6 +17,8 @@ var documentingStat: int
 var speedStat: int
 var staminaStat: int
 
+var currentStamina
+
 var firable: bool = true
 
 # 0 = FE, 1 = BE, 2 = Doc
@@ -24,6 +26,7 @@ var metricType: int
 var metricName: String
 
 func _ready() -> void:
+	currentStamina = staminaStat
 	$HoverArea.mouse_entered.connect(_on_hover_area_mouse_entered)
 	$HoverArea.mouse_exited.connect(_on_hover_area_mouse_exited)
 	TimeTool.timer.timeout.connect(work)
@@ -35,20 +38,5 @@ func _on_hover_area_mouse_exited() -> void:
 	hover_ended.emit(self)
 
 func work():
+	staminaStat -= 1
 	pass
-#	if PlayerTool.project != null:
-#		var type: int = randi_range(0,2)
-#		var amount: int
-#		match type:
-#			0: 
-#				amount = 1#frontEndStat
-#				PlayerTool.changeProjectStats(type,amount)
-#			1: 
-#				amount = 1#backEndStat
-#				PlayerTool.changeProjectStats(type,amount)
-#			2: 
-#				amount = 1#documentingStat
-#				PlayerTool.changeProjectStats(type,amount)
-#
-#		NumberVisualizer.createNumber(amount,type,self.global_position+Vector2(randf_range(-30,30),randf_range(-20,-40)))
-#		
