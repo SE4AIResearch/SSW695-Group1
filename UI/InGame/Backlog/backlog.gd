bf11203abdfd0738d14b8113e03c03e73a11b0bb
Joris <jcwilson2021@gmail.com>
2026-04-21 21:25:08 -0400

    worker headet now appears when user assigns a worker to a backlog item

--- UI/InGame/Backlog/backlog.gd
+++ UI/InGame/Backlog/backlog.gd
@@ -4,7 +4,7 @@ var backlogItem = preload("res://UI/InGame/Backlog/BacklogItem/BacklogItem.tscn"
 var backlogWorkerItem = preload("res://UI/InGame/Backlog/BacklogWorkerItem/BacklogWorkerItem.tscn")
 
 var selectedWorker: Node
-var status_message := ""
+var status_message: String = ""
 
 func _ready() -> void:
 	PlayerTool.backlogUpdated.connect(refreshBoard)
@@ -20,14 +20,15 @@ func refreshBoard() -> void:
 func _refreshWorkers() -> void:
 	for child in $WorkersScroll/Workers.get_children():
 		child.queue_free()
-	var readOnly := PlayerTool.loopPhase != PlayerTool.LOOP_PLANNING_WEEK
+	var readOnly: bool = PlayerTool.loopPhase != PlayerTool.LOOP_PLANNING_WEEK
 	for worker in PlayerTool.workers:
 		var newWorker = backlogWorkerItem.instantiate()
 		newWorker.createWorkerItem(worker)
 		newWorker.disabled = readOnly
+		newWorker.setBusyStatus(PlayerTool.selectedAssignments.has(worker.personName))
 		var assignmentId = PlayerTool.selectedAssignments.get(worker.personName, null)
 		if assignmentId != null:
-			var item := PlayerTool.getBacklogItemById(int(assignmentId))
+			var item: Dictionary = PlayerTool.getBacklogItemById(int(assignmentId))
 			if not item.is_empty():
 				newWorker.setAssignmentLabel("Assigned: " + str(item.get("name", "")))
 		newWorker.WorkerSelected.connect(workerSelected)
@@ -45,7 +46,7 @@ func _refreshItems() -> void:
 		var newItem = backlogItem.instantiate()
 		newItem.prepItem(item)
 		newItem.MetricChosen.connect(backlogSelected)
-		var readOnly := PlayerTool.loopPhase != PlayerTool.LOOP_PLANNING_WEEK
+		var readOnly: bool = PlayerTool.loopPhase != PlayerTool.LOOP_PLANNING_WEEK
 		match str(item.get("status", "backlog")):
 			"done":
 				newItem.disabled = true
@@ -58,7 +59,7 @@ func _refreshItems() -> void:
 				_add_control_to_column($BacklogScroll/Backlog, newItem)
 
 func _add_control_to_column(column: VBoxContainer, newItem: Control) -> void:
-	var itemWrapper := CenterContainer.new()
+	var itemWrapper: CenterContainer = CenterContainer.new()
 	itemWrapper.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	itemWrapper.add_child(newItem)
 	column.add_child(itemWrapper)
@@ -69,7 +70,7 @@ func updateHeader() -> void:
 		$FeedbackLabel.text = _format_feedback_message("Open the PC to choose a new project.")
 		$AdvanceWeekButton.disabled = true
 		return
-	var sprintTitle := str(PlayerTool.sprintGoal.get("title", "Sprint %d" % PlayerTool.projSprint))
+	var sprintTitle: String = str(PlayerTool.sprintGoal.get("title", "Sprint %d" % PlayerTool.projSprint))
 	$CurrentSprintLabel.text = sprintTitle + " | Week %d/%d" % [PlayerTool.projWeek, PlayerTool.project.sprintLength]
 	if PlayerTool.loopPhase == PlayerTool.LOOP_ACTIVE_WEEK:
 		status_message = "Week in progress. Assignments are locked until the timer ends."
@@ -86,7 +87,7 @@ func backlogSelected(metricButton):
 		_set_status_message("Select a worker first, then click a backlog card to assign them.")
 		return
 	var workerButton = selectedWorker
-	var result := PlayerTool.assignWorkerToItem(workerButton.heldWorker.personName, int(metricButton.heldItem.get("id", -1)))
+	var result: Dictionary = PlayerTool.assignWorkerToItem(workerButton.heldWorker.personName, int(metricButton.heldItem.get("id", -1)))
 	_set_status_message(str(result.get("reason", "")))
 	if result.get("ok", false):
 		if workerButton != null and is_instance_valid(workerButton):
@@ -100,6 +101,12 @@ func workerSelected(workerButton):
 		workerButton.button_pressed = false
 		selectedWorker = null
 		_set_status_message("Week in progress. Assignments are locked until the timer ends.")
+		return
+	if workerButton.isBusy:
+		var workerName: String = workerButton.heldWorker.personName
+		PlayerTool.unassignWorker(workerName)
+		selectedWorker = null
+		_set_status_message("%s is now free." % workerName)
 		return
 	if selectedWorker != null and selectedWorker != workerButton:
 		selectedWorker.button_pressed = false
