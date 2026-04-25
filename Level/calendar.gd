8f638dd538b80085038cbc0ef6565e16aac0a3ee
WillHandel <105618933+WillHandel@users.noreply.github.com>
2026-04-25 11:31:15 -0400

    Calendar states to guide player

--- Level/calendar.gd
+++ Level/calendar.gd
@@ -4,9 +4,11 @@ func _ready() -> void:
 	PlayerTool.projectSelected.connect(setupProjData)
 	TimeTool.timer.timeout.connect(setupProjData)
 	TimeTool.weekPassed.connect(setupProjData)
+	TimeTool.timer.timeout.connect(checkStatus)
 	PlayerTool.sprintComplete.connect(setupProjData)
 	PlayerTool.deadlineReached.connect(setupProjData)
 	setupProjData()
+	checkStatus()
 
 func setupProjData():
 	if PlayerTool.projectName == "":
@@ -19,3 +21,15 @@ func setupProjData():
 	$TextParent/weekData.text = str(PlayerTool.projWeek) + "/" + str(PlayerTool.sprintLength)
 	$weekBar.max_value = TimeTool.WEEK_DURATION_SECONDS
 	$weekBar.value = PlayerTool.weekTime
+
+func checkStatus():
+	match PlayerTool.project != null:
+		true: 
+			match PlayerTool.isWeekActive():
+				true: $TextParent/weekStatus.visible = false
+				false: 
+					$TextParent/weekStatus.visible = true
+					$TextParent/weekStatus.text = "Assign Backlog Items!"
+		false: 
+			$TextParent/weekStatus.visible = true
+			$TextParent/weekStatus.text = "Choose a Project!"
